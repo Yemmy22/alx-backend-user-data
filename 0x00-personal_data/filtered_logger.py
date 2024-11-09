d82499a24a5a519612c8f54d9477f9cbf4c158af
Yemmy22 <opeyemiadedigba20@gmail.com>
2024-11-09 14:23:26 +0000

    Fix

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -1,61 +1,32 @@
 #!/usr/bin/env python3
 """
-A main function module
+Module to connect to a secure database.
 """
 
 import os
 import mysql.connector
-from datetime import datetime
-import logging
-from typing import List
+from mysql.connector.connection import MySQLConnection
 
 
-# Set up basic logging configuration
-logging.basicConfig(
-        level=logging.INFO,
-        format='[HOLBERTON] user_data INFO %(asctime)s: %(message)s'
-        )
-logger = logging.getLogger("user_data")
+def get_db() -> MySQLConnection:
+    """
+    Connects to the MySQL database using
+    credentials from environment variables.
 
+    Returns:
+        MySQLConnection: A connection object to the database.
+    """
+    username = os.getenv("PERSONAL_DATA_DB_USERNAME")
+    password = os.getenv("PERSONAL_DATA_DB_PASSWORD")
+    host = os.getenv("PERSONAL_DATA_DB_HOST")
+    database = os.getenv("PERSONAL_DATA_DB_NAME")
 
-def get_db():
-    """Creates and returns a connector to the database."""
-    db = mysql.connector.connect(
-        host=os.getenv("PERSONAL_DATA_DB_HOST", "localhost"),
-        user=os.getenv("PERSONAL_DATA_DB_USERNAME", "root"),
-        password=os.getenv("PERSONAL_DATA_DB_PASSWORD", ""),
-        database=os.getenv("PERSONAL_DATA_DB_NAME")
+    # Connect to the MySQL database
+    connection = mysql.connector.connect(
+        user=username,
+        password=password,
+        host=host,
+        database=database
     )
-    return db
 
-
-def filter_data(row: dict, fields: List[str]) -> str:
-    """Filters sensitive data in a row."""
-    filtered = []
-
-    for key, value in row.items():
-        if key in fields:
-            filtered.append(f"{key}=***")
-        else:
-            filtered.append(f"{key}={value}")
-    return "; ".join(filtered)
-
-
-def main():
-    """Main function to retrieve and log filtered user data."""
-    db = get_db()
-    cursor = db.cursor(dictionary=True)
-    cursor.execute("SELECT * FROM users;")
-
-    sensitive_fields = ["name", "email", "phone", "ssn", "password"]
-
-    for row in cursor:
-        filtered_row = filter_data(row, sensitive_fields)
-        logger.info(filtered_row)
-
-    cursor.close()
-    db.close()
-
-
-if __name__ == "__main__":
-    main()
+    return connection
