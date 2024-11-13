6adaefa810edb587000edc06d957b8c615994ac2
Yemmy22 <opeyemiadedigba20@gmail.com>
2024-11-13 01:56:55 +0000

    Define method that extract user credentials

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -3,6 +3,7 @@
 A basic auth module
 """
 from api.v1.auth.auth import Auth
+import base64
 
 
 class BasicAuth(Auth):
@@ -24,3 +25,39 @@ class BasicAuth(Auth):
             return None
 
         return authorization_header.split("Basic ")[1]
+
+    def decode_base64_authorization_header(
+            self,
+            base64_authorization_header: str
+            ) -> str:
+        """
+        Decodes the Base64 authorization header into a UTF-8 string.
+        """
+        if base64_authorization_header is None:
+            return None
+        if not isinstance(base64_authorization_header, str):
+            return None
+        try:
+            # Decode the Base64 string and return it as a UTF-8 string
+            decoded_value = base64.b64decode(base64_authorization_header)
+            return decoded_value.decode('utf-8')
+        except (base64.binascii.Error, UnicodeDecodeError):
+            return None
+
+    def extract_user_credentials(
+            self,
+            decoded_base64_authorization_header: str
+            ) -> (str, str):
+        """
+        Extracts user email and password from the
+        decoded Base64 string.
+        """
+        if decoded_base64_authorization_header is None:
+            return None, None
+        if not isinstance(decoded_base64_authorization_header, str):
+            return None, None
+        if ':' not in decoded_base64_authorization_header:
+            return None, None
+        # Split the string by the colon and return the two parts
+        email, password = decoded_base64_authorization_header.split(":", 1)
+        return email, password
